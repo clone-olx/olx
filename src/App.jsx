3581af3d086d09ad16a0dec6c47fb6eb34c015d6
MOD Administrator <admin@M365x22087644.onmicrosoft.com>
2024-07-12 03:14:45 +0500

    Add login & SignUp and apply routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import "./App.css";
 import Home from "./components/Home";
 import ProductDetail from "./components/ProductDetail";
 import Login from "./components/Authentication/Login";
+import Signup from "./components/Authentication/Signup";
 
 function App() {
   return (
     <>
-      {/* ------------ Use all routes here ----------- */}
       
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path ="/signup" element={<Signup/>}/>
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </>
